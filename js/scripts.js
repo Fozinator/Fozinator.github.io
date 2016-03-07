2aa1d87cf51014dc8ab599b96b985e7dc79cae70
Allen <dallensanders@gmail.com>
2016-03-06 17:51:10 -0800

    stang
    stang

---
+++ js/scripts.js
@@ -0,0 +1,5 @@
+var name = prompt("Hello, what is your name?");
+var age = prompt("Nice to meet you, " + name + ". If you don't mind me asking, how old are you?");
+var bday = prompt("Just a spring chicken! What is your birthday (in month-day-year format, please)");
+var hometown = prompt("Hmmm, if I knew anything about astrology, I'd tell you what that means.  One more question, in what town were you born?");
+console.log("Once upon a time there was a person named " + name + ".  That person was " + age + " years old and born on " + bday + ".  They are from " + hometown + " but I won't hold that against them.");
